755a67a74a1dfc579e7a9ea5fb8033aeb6dc5d53
nobuhito nishihara <a01252111@yahoo.co.jp>
2022-08-07 19:20:13 +0900

    多国籍通貨対応した

--- money.py
+++ money.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 
 class Expression(metaclass=ABCMeta):
-  def plus():
-    return "test"
+  def test():
+    pass
 
-class Money(Expression):
+class Money():
   def __init__(self, amount: int, currency: str):
     self.amount = amount
     self.currency = currency
@@ -17,9 +17,17 @@ class Money(Expression):
     result = self.amount * multiplication
     return Money(result, currency)
 
-  def plus(self, another: Money) -> Money:
-    result = self.amount + another.amount
-    return Money(result, self.currency)
+  def reduce(self, currency: str, bank: Bank):
+    rate = bank.getRate(self.currency, currency)
+    amount = self.amount * rate
+    return Money(amount, currency)
+
+  #def plus(self, another: Money) -> Money:
+  #  result = self.amount + another.amount
+  #  return Money(result, self.currency)
+
+  def plus(self, another: Money) -> Expression:
+    return Sum(self, another)
 
   @staticmethod
   def doller(amount) -> Money:
@@ -30,5 +38,38 @@ class Money(Expression):
     return Money(amount, "CHF")
 
 class Bank:
-  def reduce():
-    return "test"
+  def __init__(self, rates=[]) -> None:
+    self.rates = rates
+
+  def addRate(self, from_currency, to_currency, rate):
+    h = {"from": from_currency, "to": to_currency, "rate": rate}
+    self.rates.append(h)
+
+  def getRate(self, from_currency, to_currency) -> int:
+    if(from_currency == to_currency):
+      return 1
+
+    rates = list(filter(
+      lambda x: x["from"] == from_currency and x["to"] == to_currency, self.rates))
+    return rates[-1]["rate"]
+
+  def reduce(self, source: Expression, currency) -> Money:
+    return source.reduce(currency, self)
+
+class Sum(Expression):
+  def __init__(self, augend: Money, addend: Money):
+    self.augend = augend
+    self.addend = addend
+
+  def reduce(self, currency, bank: Bank):
+    amount = self.addend.reduce(currency, bank).amount + self.augend.reduce(currency, bank).amount
+    return Money(amount, currency)
+
+class Multiple(Expression):
+  def __init__(self, augend: Money, addend: Money):
+    self.augend = augend
+    self.addend = addend
+
+  def reduce(self, currency, bank: Bank):
+    amount = self.addend.reduce(currency, bank).amount * self.augend.reduce(currency, bank).amount
+    return Money(amount, currency)
--- tests/test_money.py
+++ tests/test_money.py
@@ -1,4 +1,4 @@
-from money import Money, Expression, Bank 
+from money import Money, Expression, Bank, Multiple, Sum 
 
 def test_multiplication():
   five = Money.doller(5)
@@ -19,7 +19,36 @@ def test_currency():
   assert Money.franc(6).currency == "CHF"
 
 def test_add():
-  sum: Expression = Money.doller(5).plus(Money.doller(5))
+  sum: Sum = Sum(Money.doller(4), Money.doller(3))
   bank: Bank = Bank()
-  reduced: Money = bank.reduce(sum, "USD")
-  assert Money.doller(10).equals(reduced)
+  result = bank.reduce(sum, "USD")
+  assert result.equals(Money.doller(7))
+
+def test_expression_money():
+  money = Money.doller(5)
+  bank: Bank = Bank()
+  result = bank.reduce(money, "USD")
+  assert result.equals(Money.doller(5))
+
+def testBankRate():
+  bank: Bank = Bank()
+  bank.addRate("CHF", "USD", 2)
+  rate = bank.getRate("CHF", "USD")
+  assert rate == 2
+
+def testAddDifferentCurrency():
+  bank: Bank = Bank()
+  bank.addRate("CHF", "USD", 2)
+  result: Money = bank.reduce(Money.franc(2), "USD")
+  assert result.equals(Money.doller(4))
+  
+  sum = Sum(Money.doller(5), Money.franc(5))
+  result = bank.reduce(sum, "USD")
+  assert result.equals(Money.doller(15))
+
+def testMultipleDifferentCurrency():
+  bank = Bank()
+  bank.addRate("CHF", "USD", 2)
+  times = Multiple(Money.doller(5), Money.franc(10))
+  result = bank.reduce(times, "USD")
+  assert result.equals(Money.doller(100))
